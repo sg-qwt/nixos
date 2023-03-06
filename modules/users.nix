d36498839f25d93dfc065faf164f27043e7248ae
無名氏 <hello@edgerunners.eu.org>
2023-03-07 02:15:56 +0800

    update ssh key

--- modules/users.nix
+++ modules/users.nix
@@ -29,8 +29,8 @@ in
     users.groups.deployment = {};
     users.users.deployment = {
       isSystemUser = true;
-      openssh.authorizedKeys.keys = [
-        "no-pty ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDRy54Bxz2eul3zNZLq+Vw9DZPs7DCLTh9HsQdl8FFEw7co92C2a0+Ih5WxF2rh7Z89Em9yyHvMLu6iz8Zv78AY6CJVHgxeDzEcf3XtNanIbDMyD2omLn6MpnLCJWQCK0uYQtj4W/aCPLlSAJEyTUPhmy/syg/9An4xgx33YcWOyXNks24b4WttSM+tQ2qpem1yVnnV9imziYsdCEU34dacmyVY4ayWPxSWEEcxFIKcp/s10bbdYoIL/Zc88vRbBwZwk1hVOgJ+D8DFpIxHy+Uyr/KgRQCKp4Bju6AJUDYq6jKS3Bv/4zS+4ZF3ArV/AUxP5KxEPXsjhNic34uorPOcxTyJcXe7lylSYjD3h/I8hthmleiYCcOOHCKFjPSI2BpuXpG4T49tJ0jHMA45cP2I13jDoWbSYu8mKglarutykSbj6PjTfTmIXUSUwXkXW4052bWiaxReDySYjUw+anJZLXX/EqFyN/Eiw2CG/l3o8uKuASeqbmn+z/bq30FghAczHbDR3W/AOoDybqxSCle2dRzESmabHkhQep85BZJ8D1bNy6TVvIgAzsLPzEWWi/f2i6XAcmDHSaaqeq+zffxje9XxLlDo3Rb4hsTSrG76Qt0VeWC6DwoEFPDgorXTGN7c9yfxftFlFz15jzWVLUdS95hfqLVpqDAs3nLzGrjiKQ== (none)"
+      openssh.authorizedKeys.keyFiles = [
+        ../resources/keys/ssh-me.pub
       ];
       group = "deployment";
       shell = pkgs.bash;
@@ -41,8 +41,8 @@ in
       description = "me";
       extraGroups = [ "wheel" "networkmanager" "audio" "video" "docker" "systemd-journal" ] ++ cfg.extraGroups;
       passwordFile = config.sops.secrets.me-password.path;
-      openssh.authorizedKeys.keys = [
-        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDRy54Bxz2eul3zNZLq+Vw9DZPs7DCLTh9HsQdl8FFEw7co92C2a0+Ih5WxF2rh7Z89Em9yyHvMLu6iz8Zv78AY6CJVHgxeDzEcf3XtNanIbDMyD2omLn6MpnLCJWQCK0uYQtj4W/aCPLlSAJEyTUPhmy/syg/9An4xgx33YcWOyXNks24b4WttSM+tQ2qpem1yVnnV9imziYsdCEU34dacmyVY4ayWPxSWEEcxFIKcp/s10bbdYoIL/Zc88vRbBwZwk1hVOgJ+D8DFpIxHy+Uyr/KgRQCKp4Bju6AJUDYq6jKS3Bv/4zS+4ZF3ArV/AUxP5KxEPXsjhNic34uorPOcxTyJcXe7lylSYjD3h/I8hthmleiYCcOOHCKFjPSI2BpuXpG4T49tJ0jHMA45cP2I13jDoWbSYu8mKglarutykSbj6PjTfTmIXUSUwXkXW4052bWiaxReDySYjUw+anJZLXX/EqFyN/Eiw2CG/l3o8uKuASeqbmn+z/bq30FghAczHbDR3W/AOoDybqxSCle2dRzESmabHkhQep85BZJ8D1bNy6TVvIgAzsLPzEWWi/f2i6XAcmDHSaaqeq+zffxje9XxLlDo3Rb4hsTSrG76Qt0VeWC6DwoEFPDgorXTGN7c9yfxftFlFz15jzWVLUdS95hfqLVpqDAs3nLzGrjiKQ== (none)"
+      openssh.authorizedKeys.keyFiles = [
+        ../resources/keys/ssh-me.pub
       ];
     };
 
