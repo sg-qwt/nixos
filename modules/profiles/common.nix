b2202ac57d6afefbbe9627514849b44feca82dd6
無名氏 <hello@edgerunners.eu.org>
2023-03-10 18:02:43 +0800

    remove duplicate openssh

--- modules/profiles/common.nix
+++ modules/profiles/common.nix
@@ -55,24 +55,4 @@ helpers.mkProfile s "common"
       dates = "weekly";
     };
   };
-
-  services.openssh = {
-    enable = true;
-    settings = {
-      kbdInteractiveAuthentication = false;
-      passwordAuthentication = false;
-      permitRootLogin = lib.mkForce "no";
-    };
-    hostKeys = [
-      {
-        bits = 4096;
-        path = "/etc/ssh/ssh_host_rsa_key";
-        type = "rsa";
-      }
-      {
-        path = "/etc/ssh/ssh_host_ed25519_key";
-        type = "ed25519";
-      }
-    ];
-  };
 }
