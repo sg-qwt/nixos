5b6a036c17d5bd55ade038e7ac5feb886bf6f399
無名氏 <hello@edgerunners.eu.org>
2023-03-30 19:28:45 +0800

    refactor: pass in yacd url

--- packages/yacd-meta/default.nix
+++ packages/yacd-meta/default.nix
@@ -1,14 +1,13 @@
-{ stdenv, inputs, lib }:
+{ stdenv, inputs, yacd-url ? "127.0.0.1:9090" }:
 stdenv.mkDerivation rec {
   pname = "yacd-meta";
   version = inputs.yacd-meta.shortRev;
   src = inputs.yacd-meta;
 
   prePatch =
-    let port = (lib.importJSON (./. + "/../../config/data.json")).ports.clash-meta-api; in
     ''
       substituteInPlace ./index.html \
-        --replace "127.0.0.1:9090" "127.0.0.1:${toString port}"
+        --replace "127.0.0.1:9090" "${yacd-url}"
     '';
 
   installPhase = ''
