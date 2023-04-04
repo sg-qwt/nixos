bb4aafc5cf8f64c7db8e67899e6b5746ba4b04ce
無名氏 <hello@edgerunners.eu.org>
2023-04-04 17:48:16 +0800

    feat: manage edg dns with terraform

---
+++ infra/cf_edg.tf
@@ -0,0 +1,58 @@
+data "cloudflare_zones" "edg" {
+  filter {
+    name = local.fqdn.edg
+  }
+}
+
+locals {
+  edg_zone_id = one(data.cloudflare_zones.edg.zones[*].id)
+}
+
+resource "cloudflare_record" "root" {
+  for_each = {
+    a = {
+      name  = "@"
+      type  = "A"
+      value = local.dui_ipv4
+    }
+    aaaa = {
+      name  = "@"
+      type  = "AAAA"
+      value = local.dui_ipv6
+    }
+    dui = {
+      name  = "dui"
+      type  = "CNAME"
+      value = local.fqdn.edg
+    }
+  }
+
+  allow_overwrite = true
+  zone_id         = local.edg_zone_id
+  name            = each.value.name
+  value           = each.value.value
+  type            = each.value.type
+  proxied         = false
+}
+
+resource "cloudflare_email_routing_settings" "edg" {
+  zone_id = local.edg_zone_id
+  enabled = true
+}
+
+variable "fw_email" { type = string }
+
+resource "cloudflare_email_routing_catch_all" "all" {
+  zone_id = local.edg_zone_id
+  name    = "forward all"
+  enabled = true
+
+  matcher {
+    type = "all"
+  }
+
+  action {
+    type  = "forward"
+    value = [var.fw_email]
+  }
+}
