e7f38472b83f997c8470df47e29f7fc148d43e9d
無名氏 <hello@edgerunners.eu.org>
2023-04-15 02:58:00 +0800

    chore: move bash code

--- config/scripts/bashrc.nix
+++ config/scripts/bashrc.nix
@@ -1,28 +1,4 @@
 ''
-  enable-proxy() {
-    PROXY_HOST="localhost"
-    PROXY_PORT="7890"
-
-    export HTTP_PROXY="http://$PROXY_HOST:$PROXY_PORT/"
-    export http_proxy="$HTTP_PROXY"
-
-    export HTTPS_PROXY="http://$PROXY_HOST:$PROXY_PORT/"
-    export https_proxy="$HTTPS_PROXY"
-
-    export NO_PROXY="localhost, 127.0.0.0/8, ::1"
-    export no_proxy="$NO_PROXY"
-
-    echo "HTTP Proxy Enabled!"
-  }
-
-  disable-proxy() {
-    unset HTTP_PROXY http_proxy
-    unset HTTPS_PROXY https_proxy
-    unset NO_PROXY no_proxy
-
-    echo "HTTP Proxy Disabled!"
-  }
-
   deploy() {
     nixos-rebuild --target-host deploy@"$1" --flake $MYOS_FLAKE#"$1" --use-remote-sudo switch
   }
--- modules/profiles/clashm.nix
+++ modules/profiles/clashm.nix
@@ -110,5 +110,34 @@ in
           };
         };
       };
+
+      home-manager.users."${config.myos.users.mainUser}" = {
+
+        programs.bash.bashrcExtra = ''
+          enable-proxy() {
+            PROXY_HOST="localhost"
+            PROXY_PORT="${toString ports.clash-meta-mixed}"
+
+            export HTTP_PROXY="http://$PROXY_HOST:$PROXY_PORT/"
+            export http_proxy="$HTTP_PROXY"
+
+            export HTTPS_PROXY="http://$PROXY_HOST:$PROXY_PORT/"
+            export https_proxy="$HTTPS_PROXY"
+
+            export NO_PROXY="localhost, 127.0.0.0/8, ::1"
+            export no_proxy="$NO_PROXY"
+
+            echo "HTTP Proxy Enabled!"
+          }
+
+          disable-proxy() {
+            unset HTTP_PROXY http_proxy
+            unset HTTPS_PROXY https_proxy
+            unset NO_PROXY no_proxy
+
+            echo "HTTP Proxy Disabled!"
+          }
+        '';
+      };
     };
 }
