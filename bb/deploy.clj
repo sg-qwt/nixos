3730fa36ac83dc199d4539324e1a6313682a4cd3
無名氏 <hello@edgerunners.eu.org>
2023-04-19 00:52:44 +0800

    feat: bb deploy

---
+++ bb/deploy.clj
@@ -0,0 +1,36 @@
+(ns deploy
+  (:require [babashka.process :as bp :refer [shell]]
+            [cheshire.core :as json]))
+
+(def ts-host-ids [:ge :zheng :lei])
+(def fqdn "edgerunners.eu.org")
+(def user "deploy")
+
+(defn- ts-hosts
+  [ids]
+  (->> ids
+       (mapv (fn [id] {:id id :host (str (name id) ".ts." fqdn) :user user}))))
+
+(def hosts
+  (conj (ts-hosts ts-host-ids) {:id :dui :host (str "dui." fqdn) :user user}))
+
+(defn deploy-host
+  [{:keys [id host user]}]
+  (let [flake (or (System/getenv "MYOS_FLAKE") "github:sg-qwt/nixos")]
+    (println ">>> deploy: " id)
+    (println ">>> host: " host)
+    (println ">>> user: " user)
+    (println ">>> flake: " flake)
+    (shell "nixos-rebuild"
+           "--target-host" (str user "@" host)
+           "--flake" (str flake "#" (name id))
+           "--use-remote-sudo" "switch")
+    (println ">>> deploy success")))
+
+(defn deploy
+  [name]
+  (if-some [host (->> hosts
+                      (filter (fn [host] (= (:id host) (keyword name))))
+                      first)]
+    (deploy-host host)
+    (println ">>> no host found: " name)))
