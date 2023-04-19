1a4ec69402eb7ca5130d61b42e97cb32cf82f625
無名氏 <hello@edgerunners.eu.org>
2023-04-19 14:33:59 +0800

    refactor: minor

--- images.nix
+++ images.nix
@@ -1,10 +1,14 @@
 { nixpkgs, system, rootPath, pkgs, jovian }:
+let
+  mPath = "${nixpkgs}/nixos/modules";
+  lib = nixpkgs.lib;
+in
 {
-  azure-image = (nixpkgs.lib.nixosSystem {
+  azure-image = (lib.nixosSystem {
     inherit system;
     specialArgs = { inherit pkgs rootPath; };
     modules = [
-      "${nixpkgs}/nixos/modules/virtualisation/azure-image.nix"
+      "${mPath}/virtualisation/azure-image.nix"
       ./modules/mixins/deploy.nix
       ./modules/mixins/azurebase.nix
       ({ pkgs, modulesPath, lib, config, ... }: {
@@ -23,14 +27,14 @@
     ];
   }).config.system.build.azureImage;
 
-  gnome-image = (nixpkgs.lib.nixosSystem {
+  gnome-image = (lib.nixosSystem {
     inherit system;
     modules = [
-      "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-graphical-gnome.nix"
+      "${mPath}/installer/cd-dvd/installation-cd-graphical-gnome.nix"
     ];
   }).config.system.build.isoImage;
 
-  deck-minimal-image = (nixpkgs.lib.nixosSystem {
+  deck-minimal-image = (lib.nixosSystem {
     inherit system;
     specialArgs = {
       inherit rootPath;
@@ -38,7 +42,7 @@
     };
     modules = [
       "${jovian}/modules"
-      "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+      "${mPath}/installer/cd-dvd/installation-cd-minimal.nix"
       ./modules/mixins/deploy.nix
       ({ lib, config, ... }: {
         jovian.devices.steamdeck.enable = true;
