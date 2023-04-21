a0c28f677a9804893167ca1ea19c8bc01d0762db
無名氏 <hello@edgerunners.eu.org>
2023-04-22 00:48:11 +0800

    fix: include patch that do not flush nftables

--- modules/profiles/tailscale.nix
+++ modules/profiles/tailscale.nix
@@ -1,30 +1,42 @@
 s@{ config, pkgs, lib, helpers, self, ... }:
-helpers.mkProfile s "tailscale"
-{
-  services.tailscale.enable = true;
-
-  systemd.services.tailscale-setup = {
-    script = ''
-      sleep 10
-
-      if tailscale status; then
-        echo "tailscale already up, skip"
-      else
-        echo "tailscale down, login using auth key"
-        tailscale up --auth-key "file:${config.sops.secrets.tailscale_tailnet_key.path}"
-      fi
-    '';
-    serviceConfig = {
-      Type = "oneshot";
-      RemainAfterExit = true;
+helpers.mkProfile s "tailscale" (
+  let
+    interface = "tailscale0";
+    port = config.myos.data.ports.tailscaled;
+  in
+  {
+    services.tailscale = {
+      enable = true;
+      interfaceName = interface;
+      port = port;
     };
-    path = [ config.services.tailscale.package ];
-    after = [ "tailscaled.service" ];
-    requiredBy = [ "tailscaled.service" ];
-  };
 
-  sops.secrets.tailscale_tailnet_key = {
-    sopsFile = self + "/secrets/tfout.json";
-    restartUnits = [ "tailscale-setup.service" ];
-  };
-}
+    networking.firewall.allowedUDPPorts = [ port ];
+    networking.firewall.trustedInterfaces = [ interface ];
+
+    systemd.services.tailscale-setup = {
+      script = ''
+        sleep 10
+
+        if tailscale status; then
+          echo "tailscale already up, skip"
+        else
+          echo "tailscale down, login using auth key"
+          tailscale up --auth-key "file:${config.sops.secrets.tailscale_tailnet_key.path}"
+        fi
+      '';
+      serviceConfig = {
+        Type = "oneshot";
+        RemainAfterExit = true;
+      };
+      path = [ config.services.tailscale.package ];
+      after = [ "tailscaled.service" ];
+      requiredBy = [ "tailscaled.service" ];
+    };
+
+    sops.secrets.tailscale_tailnet_key = {
+      sopsFile = self + "/secrets/tfout.json";
+      restartUnits = [ "tailscale-setup.service" ];
+    };
+  }
+)
