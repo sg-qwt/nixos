2cf9d14f05192a177c728e1098c0ecc39bba24e6
無名氏 <hello@edgerunners.eu.org>
2023-06-14 14:53:27 +0800

    chore: update dicts

--- modules/profiles/sdcv-with-dicts.nix
+++ modules/profiles/sdcv-with-dicts.nix
@@ -1,24 +1,19 @@
 s@{ config, pkgs, lib, helpers, self, ... }:
 helpers.mkProfile s "sdcv-with-dicts" (
   let
+    handict = pkgs.fetchzip {
+      url = "http://download.huzheng.org/zh_CN/stardict-chibigenc-2.4.2.tar.bz2";
+      sha256 = "sha256-n1QFhUUWpsYyknPmaY3mGqfMXUj6OysUsZNWvjFdu9k=";
+    };
     oald = pkgs.fetchzip {
       url = "http://download.huzheng.org/dict.org/stardict-oald-2.4.2.tar.bz2";
       sha256 = "sha256-7rvIkDj6qXBJwPV4lL42IbpDCMz4t8tJ8LxHf4a2MpE=";
-    };
-    kangxi = pkgs.fetchzip {
-      url = "http://download.huzheng.org/zh_CN/stardict-kangxitext-2.4.2.tar.bz2";
-      sha256 = "sha256-6b2OiYXgdkCHPZ+h0zrTwyEujBuwiugCWO6VV9jkGDI=";
-    };
-    moedict = pkgs.fetchzip {
-      url = "file://${(self + "/resources/dicts/moedict.tar.gz")}";
-      sha256 = "sha256-G8jr+ki59qc8guiwd7XbfUPrrLxWkq6FpanEGwVhwfU=";
     };
     dictFiles = pkgs.symlinkJoin {
       name = "sdcv-dict-files";
       paths = [
         oald
-        kangxi
-        moedict
+        handict
       ];
     };
     mySdcv = pkgs.writeScriptBin "sdcv" ''
