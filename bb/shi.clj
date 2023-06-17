4b71b39f85754a3c22a8c1b4e5769bf04459ca12
無名氏 <hello@edgerunners.eu.org>
2023-06-17 23:54:40 +0800

    chore: fmt

--- bb/shi.clj
+++ bb/shi.clj
@@ -3,13 +3,12 @@
 
 (def shi-file-path (first *command-line-args*))
 
-(println
- (->> (str/split (slurp shi-file-path) #"\n\n")
-      (filter (fn [l]
-                (and (str/starts-with? l "《")
-                     (> (count (str/split-lines l)) 1))))
-      rand-nth
-      str/split-lines
-      (map (fn [l] (str ";; " l)))
-      (str/join \newline)))
+(println (->> (str/split (slurp shi-file-path) #"\n\n")
+              (filter (fn [l]
+                        (and (str/starts-with? l "《")
+                             (> (count (str/split-lines l)) 1))))
+              rand-nth
+              str/split-lines
+              (map (fn [l] (str ";; " l)))
+              (str/join \newline)))
 
