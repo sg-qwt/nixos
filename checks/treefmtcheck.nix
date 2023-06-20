0c6a4e0eb6ccea57a5da22abcc6158a63d822300
無名氏 <hello@edgerunners.eu.org>
2023-06-21 05:05:09 +0800

    feat: add nix flake check for treefmt

---
+++ checks/treefmtcheck.nix
@@ -0,0 +1,16 @@
+{
+  pkgs,
+  mytreefmt,
+  self
+}:
+pkgs.runCommandNoCC "treefmt" {
+  nativeBuildInputs = [
+    mytreefmt
+  ];
+} ''
+  # keep timestamps so that treefmt is able to detect mtime changes
+  cp --no-preserve=mode --preserve=timestamps -r ${self} source
+  cd source
+  HOME=$TMPDIR treefmt --no-cache --fail-on-change
+  touch $out
+''
