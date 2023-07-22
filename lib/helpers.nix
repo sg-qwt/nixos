86537155e1cb6371fbcb27a7d37f2c14b03755d6
無名氏 <hello@edgerunners.eu.org>
2023-07-23 01:35:46 +0800

    chore: fmt

--- lib/helpers.nix
+++ lib/helpers.nix
@@ -1,10 +1,10 @@
 { sources, self, nixpkgs }:
 rec {
   patchDesktop = pkgs: pkg: appName: from: to: lib.hiPrio
-    (pkgs.runCommand "$patched-desktop-entry-for-${appName}" {}
+    (pkgs.runCommand "$patched-desktop-entry-for-${appName}" { }
       ''
-      ${pkgs.coreutils}/bin/mkdir -p $out/share/applications
-      ${pkgs.gnused}/bin/sed 's#${from}#${to}#g' < ${pkg}/share/applications/${appName}.desktop > $out/share/applications/${appName}.desktop
+        ${pkgs.coreutils}/bin/mkdir -p $out/share/applications
+        ${pkgs.gnused}/bin/sed 's#${from}#${to}#g' < ${pkg}/share/applications/${appName}.desktop > $out/share/applications/${appName}.desktop
       '');
 
   profile-list =
