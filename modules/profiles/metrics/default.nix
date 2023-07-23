9f7da9ac064be4e5920252d117dfc086c8113556
無名氏 <hello@edgerunners.eu.org>
2023-07-23 23:09:42 +0800

    feat: telegraf init

---
+++ modules/profiles/metrics/default.nix
@@ -0,0 +1,39 @@
+s@{ config, pkgs, lib,  self, ... }:
+let
+  inherit (config.myos.data) ports fqdn path;
+in
+lib.mkProfile s "metrics" {
+  services.telegraf = {
+    enable = true;
+    extraConfig = {
+      agent.interval = "60s";
+      inputs = {
+        cpu = { };
+        disk = { };
+        diskio = { };
+        mem = { };
+        net = { };
+        processes = { };
+        system = { };
+        systemd_units = { };
+        dns_query = {
+          servers = [
+            "1.1.1.1"
+            "8.8.8.8"
+          ];
+          domains = [ fqdn.edg ];
+          record_type = "A";
+          timeout = "2s";
+        };
+      };
+      outputs = {
+        prometheus_client = {
+          listen = ":${toString ports.telegraf-prometheus-client}";
+          metric_version = 2;
+          path = path.metrics;
+        };
+      };
+    };
+  };
+
+}
