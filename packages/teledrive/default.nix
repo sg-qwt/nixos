9c5892f4896076daca8df0b5ac4f3f5b90d80479
無名氏 <hello@edgerunners.eu.org>
2023-07-27 01:13:32 +0800

    feat: new pkg teledrive

---
+++ packages/teledrive/default.nix
@@ -0,0 +1,112 @@
+{ fetchFromGitHub
+, lib
+, fetchYarnDeps
+, nodejs
+, yarn2nix-moretea
+, makeWrapper
+, prisma-engines
+, applyPatches
+, symlinkJoin
+, ...
+}:
+let
+  yarnLock = ./yarn.lock;
+  workspace =
+    (yarn2nix-moretea.mkYarnWorkspace {
+      pname = "teledrive";
+      version = "ade6b9488953684c478167bbfed3e9868798a814";
+      src = applyPatches {
+        src = fetchFromGitHub ({
+          owner = "mgilangjanuar";
+          repo = "teledrive";
+          rev = "ade6b9488953684c478167bbfed3e9868798a814";
+          fetchSubmodules = false;
+          sha256 = "sha256-akkvHesKkBQNBZD+SZ1oJd9aMQHBCL7TI7cMfxe+2go=";
+        });
+        postPatch = ''
+          substituteInPlace api/package.json --replace '^4.4.2' '^4.1.5'
+
+          substituteInPlace api/src/index.ts --replace "path.join(__dirname, '..', '..', 'web', 'build')" 'process.env.WEB_DIR'
+
+          substituteInPlace api/src/index.ts --replace "path.join(__dirname, '..', '..','web', 'build', 'index.html')" "path.join(process.env.WEB_DIR, 'index.html')"
+        '';
+
+      };
+
+      inherit yarnLock;
+
+      offlineCache = fetchYarnDeps {
+        inherit yarnLock;
+        sha256 = "sha256-VfujjED1WwCkjabnD18aAS89usQwV2G6fP3woTamAXE=";
+      };
+
+      nativeBuildInputs = [
+        makeWrapper
+      ];
+
+      packageOverrides = {
+        web = {
+          buildPhase = ''
+            runHook preBuild
+
+            export HOME=$(mktemp -d)
+            export WRITABLE_NODE_MODULES="$(pwd)/tmp"
+
+            mkdir -p "$WRITABLE_NODE_MODULES"
+            cd deps/web
+            node_modules="$(readlink node_modules)"
+            rm node_modules
+            mkdir -p "$WRITABLE_NODE_MODULES"/.cache
+            cp -r $node_modules/* "$WRITABLE_NODE_MODULES"
+
+            mkdir -p "$WRITABLE_NODE_MODULES"/.bin
+            for x in "$node_modules"/.bin/*; do
+             ln -sfv "$node_modules"/.bin/"$(readlink "$x")" "$WRITABLE_NODE_MODULES"/.bin/"$(basename "$x")"
+            done
+
+            ln -sfv "$WRITABLE_NODE_MODULES" node_modules
+            cd ../..
+
+            yarn --offline build 
+
+            runHook postBuild
+          '';
+        };
+        api = {
+
+          buildPhase = ''
+            runHook preBuild
+            export PRISMA_MIGRATION_ENGINE_BINARY=${prisma-engines}/bin/migration-engine
+            export PRISMA_QUERY_ENGINE_BINARY=${prisma-engines}/bin/query-engine
+            export PRISMA_QUERY_ENGINE_LIBRARY=${prisma-engines}/lib/libquery_engine.node
+            export HOME=$(mktemp -d)
+            yarn --offline build 
+            runHook postBuild
+          '';
+
+          postInstall = ''
+            makeWrapper ${nodejs}/bin/node $out/bin/teledrive \
+              --add-flags $out/libexec/api/deps/api/dist/index.js \
+              --set PRISMA_MIGRATION_ENGINE_BINARY "${prisma-engines}/bin/migration-engine" \
+              --set PRISMA_QUERY_ENGINE_BINARY "${prisma-engines}/bin/query-engine" \
+              --set PRISMA_QUERY_ENGINE_LIBRARY "${prisma-engines}/lib/libquery_engine.node"
+          '';
+        };
+      };
+
+      meta = with lib; {
+        description = "teledrive";
+        homepage = "https://github.com/mgilangjanuar/teledrive";
+        license = licenses.gpl3;
+      };
+    });
+in
+symlinkJoin {
+  name = "teledrive";
+  paths = [ workspace.api ];
+  buildInputs = [ makeWrapper ];
+  postBuild = ''
+    wrapProgram $out/bin/teledrive \
+      --set WEB_DIR "${workspace.web}/libexec/web/deps/web/build"
+  '';
+}
