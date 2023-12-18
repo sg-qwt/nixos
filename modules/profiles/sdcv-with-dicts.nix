33518117b8d51611c447ac20e458dac6632bbfbf
無名氏 <hello@edgerunners.eu.org>
2023-12-18 21:49:24 +0800

    chore: comment out dict

--- modules/profiles/sdcv-with-dicts.nix
+++ modules/profiles/sdcv-with-dicts.nix
@@ -1,19 +1,19 @@
 s@{ config, pkgs, lib, self, ... }:
 lib.mkProfile s "sdcv-with-dicts" (
   let
-    handict = pkgs.fetchzip {
-      url = "http://download.huzheng.org/zh_CN/stardict-chibigenc-2.4.2.tar.bz2";
-      sha256 = "sha256-n1QFhUUWpsYyknPmaY3mGqfMXUj6OysUsZNWvjFdu9k=";
-    };
-    oald = pkgs.fetchzip {
-      url = "http://download.huzheng.org/dict.org/stardict-oald-2.4.2.tar.bz2";
-      sha256 = "sha256-7rvIkDj6qXBJwPV4lL42IbpDCMz4t8tJ8LxHf4a2MpE=";
-    };
+    # handict = pkgs.fetchzip {
+    #   url = "http://download.huzheng.org/zh_CN/stardict-chibigenc-2.4.2.tar.bz2";
+    #   sha256 = "sha256-n1QFhUUWpsYyknPmaY3mGqfMXUj6OysUsZNWvjFdu9k=";
+    # };
+    # oald = pkgs.fetchzip {
+    #   url = "http://download.huzheng.org/dict.org/stardict-oald-2.4.2.tar.bz2";
+    #   sha256 = "sha256-7rvIkDj6qXBJwPV4lL42IbpDCMz4t8tJ8LxHf4a2MpE=";
+    # };
     dictFiles = pkgs.symlinkJoin {
       name = "sdcv-dict-files";
       paths = [
-        oald
-        handict
+        # oald
+        # handict
       ];
     };
     mySdcv = pkgs.writeScriptBin "sdcv" ''
