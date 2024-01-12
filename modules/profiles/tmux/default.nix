be149112c959f7a286362b92b8c7f474e75b01a3
無名氏 <hello@edgerunners.eu.org>
2024-01-12 19:49:35 +0800

    chore: migrate to programs.tmux

--- modules/profiles/tmux/default.nix
+++ modules/profiles/tmux/default.nix
@@ -1,16 +1,56 @@
-s@{ config, pkgs, lib, self, ... }:
+s@{ lib, ... }:
 lib.mkProfile s "tmux"
 {
-  environment = {
-    systemPackages = with pkgs; [
-      tmux
-      (if config.myos.wayland.enable then wl-clipboard else xclip)
-    ];
+  programs.tmux = {
+    enable = true;
+    keyMode = "vi";
+    baseIndex = 1;
+    newSession = true;
+    shortcut = "a";
+    historyLimit = 10000;
+    terminal = "tmux-256color";
+    customPaneNavigationAndResize = true;
+    extraConfig = ''
+      bind | split-window -h
+      bind - split-window -v
 
-    etc."tmux.conf".source =
-      pkgs.substituteAll {
-        src = ./tmux.conf;
-        cmd = (if config.myos.wayland.enable then "wl-copy" else "xclip -in -selection clipboard");
-      };
+      bind -r C-h select-window -t :-
+      bind -r C-l select-window -t :+
+
+      # Change status bar colors
+      set -g status-fg white
+      set -g status-bg black
+
+      # Change window list colors
+      setw -g window-status-style fg=cyan,bg=default,dim
+      setw -g window-status-current-style fg=white,bg=red,bright
+
+      # Change pane divider colors
+      set -g pane-border-style fg=green,bg=black
+      set -g pane-active-border-style fg=white,bg=yellow
+
+      # Customize the command line
+      set -g message-style fg=white,bg=black,bright
+
+      # Customize status bar
+      set -g status-left-length 40
+      set -g status-left "#[fg=green]Session: #S #[fg=yellow]#I #[fg=cyan]#P"
+      set -g status-right "#[fg=cyan]%d %b %R"
+
+      # Center the window list
+      set -g status-justify centre
+
+      # Identify activity in other windows
+      setw -g monitor-activity on
+      set -g visual-activity on
+
+      # Remap vi style copy and paste
+      unbind [
+      bind C-[ copy-mode
+      unbind p
+      bind p paste-buffer
+      bind-key -T copy-mode-vi v send-keys -X begin-selection
+      bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
+    '';
   };
 }
