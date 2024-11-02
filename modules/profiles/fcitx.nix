1b827b267f306bec3a568bf6549bbcd7a6925d87
無名氏 <hello@edgerunners.eu.org>
2024-11-03 04:56:13 +0800

    fcitx5: add pinyin config

--- modules/profiles/fcitx.nix
+++ modules/profiles/fcitx.nix
@@ -12,15 +12,25 @@ lib.mkProfile s "fcitx"
         pkgs-latest.fcitx5-pinyin-zhwiki
       ];
       ignoreUserConfig = true;
-      settings.inputMethod = {
-        "Groups/0" = {
-          "Name" = "Default";
-          "Default Layout" = "us";
-          "DefaultIM" = "pinyin";
+      settings = {
+        inputMethod = {
+          "Groups/0" = {
+            "Name" = "Default";
+            "Default Layout" = "us";
+            "DefaultIM" = "pinyin";
+          };
+          "Groups/0/Items/0"."Name" = "keyboard-us";
+          "Groups/0/Items/1"."Name" = "pinyin";
+          "GroupOrder"."0" = "Default";
         };
-        "Groups/0/Items/0"."Name" = "keyboard-us";
-        "Groups/0/Items/1"."Name" = "pinyin";
-        "GroupOrder"."0" = "Default";
+        addons = {
+          pinyin.globalSection = {
+            EmojiEnabled = "True";
+            CloudPinyinEnabled = "False";
+            FirstRun = "False";
+            PageSize = 9;
+          };
+        };
       };
     };
   };
