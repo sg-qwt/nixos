c0b5b6cfe41d842d80aa1e7377d33da279dd9db2
無名氏 <hello@edgerunners.eu.org>
2024-11-04 02:02:04 +0800

    chore: update

--- lib/helpers.nix
+++ lib/helpers.nix
@@ -44,26 +44,6 @@ rec {
                 value = (prev.callPackage (self + "/packages/${pkgname}") args);
               })
             (builtins.attrNames (builtins.readDir (self + "/packages")))));
-      sway-unwrapped =
-        (prev.sway-unwrapped.override { wlroots = final.wlroots_0_18; }).overrideAttrs
-          (
-            finalAttrs: prevAttrs: {
-              version = "1.10";
-              src = prevAttrs.src.override {
-                hash = "sha256-PzeU/niUdqI6sf2TCG19G2vNgAZJE5JCyoTwtO9uFTk=";
-              };
-
-              mesonFlags =
-                let
-                  inherit (final.lib.strings) mesonEnable mesonOption;
-                  sd-bus-provider = if finalAttrs.systemdSupport then "libsystemd" else "basu";
-                in
-                  [
-                    (mesonOption "sd-bus-provider" sd-bus-provider)
-                    (mesonEnable "tray" finalAttrs.trayEnabled)
-                  ];
-            }
-          );
     };
 
   packages = pkgs:
