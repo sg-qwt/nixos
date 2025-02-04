ec00de52e07fd3976befea30ea15852910fc2755
無名氏 <hello@edgerunners.eu.org>
2025-02-04 20:12:09 +0800

    chore: bump system

--- hosts/zheng/hardware-configuration.nix
+++ hosts/zheng/hardware-configuration.nix
@@ -53,6 +53,5 @@
     size = 1024 * 20;
   }];
 
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
