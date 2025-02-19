93d78dd0b75367807e30021938523c01317cd3a6
無名氏 <hello@edgerunners.eu.org>
2025-02-19 20:18:23 +0800

    emacs: add dired-preview

--- packages/qqqemacs/default.nix
+++ packages/qqqemacs/default.nix
@@ -54,9 +54,6 @@ let
       pdf-tools
       nov
 
-      # TODO replacde with dired-preview
-      peep-dired
-
       yasnippet
       yasnippet-capf
 
@@ -78,6 +75,13 @@ let
       corfu
       nftables-mode
       jarchive
+      (dired-preview.overrideAttrs (old: {
+        version = "0.4.0";
+        src = pkgs.fetchurl {
+          url = "https://elpa.gnu.org/packages/dired-preview-0.4.0.tar";
+          sha256 = "9HySQYFGsuM6XORsOqkQUk7zekeBkcbyEKjXDN7bQNs=";
+        };
+      }))
     ]));
 
   deps = with pkgs; [
