98d90286df588580336348d96c2db079baa0b6bd
無名氏 <hello@edgerunners.eu.org>
2025-06-11 09:02:08 +0800

    fix: excludes from treefmt

--- lib/treefmt.nix
+++ lib/treefmt.nix
@@ -1,5 +1,10 @@
 {
   projectRootFile = "flake.nix";
+  settings.global.excludes = [
+    "*.age"
+    "secrets/cache/*"
+    "resources/*"
+  ];
   programs.nixpkgs-fmt.enable = true;
   programs.terraform.enable = true;
   programs.zprint = {
