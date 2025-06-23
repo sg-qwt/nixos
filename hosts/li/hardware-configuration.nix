6fd550549c1bce04ff8a48e63c28bf8bd7d19613
無名氏 <hello@edgerunners.eu.org>
2025-06-23 12:55:09 +0800

    li: use latest and open nvidia

--- hosts/li/hardware-configuration.nix
+++ hosts/li/hardware-configuration.nix
@@ -33,17 +33,11 @@
     nvidia = {
       modesetting.enable = true;
       powerManagement.enable = true;
-      open = false;
+      open = true;
       nvidiaSettings = true;
       dynamicBoost.enable = true;
 
-      package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-        version = "570.133.07";
-        sha256_64bit = "sha256-LUPmTFgb5e9VTemIixqpADfvbUX1QoTT2dztwI3E3CY=";
-        openSha256 = "sha256-9l8N83Spj0MccA8+8R1uqiXBS0Ag4JrLPjrU3TaXHnM=";
-        settingsSha256 = "sha256-XMk+FvTlGpMquM8aE8kgYK2PIEszUZD2+Zmj2OpYrzU=";
-        usePersistenced = false;
-      };
+      package = config.boot.kernelPackages.nvidiaPackages.latest;
     };
   };
 
