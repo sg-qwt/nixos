7affb2ae0fcf3d2fd18396fe76121cd23328bc01
無名氏 <hello@edgerunners.eu.org>
2025-06-25 17:15:48 +0800

    feat: add chromium policy

--- modules/profiles/desktop-apps.nix
+++ modules/profiles/desktop-apps.nix
@@ -1,4 +1,7 @@
 s@{ pkgs, lib, ... }:
+let
+  search-url = "https://www.google.co.uk";
+in
 lib.mkProfile s "desktop-apps"
 {
   # file manager
@@ -6,6 +9,11 @@ lib.mkProfile s "desktop-apps"
   programs.thunar.enable = true;
 
   programs.chromium = {
+    enable = true;
+    homepageLocation = search-url;
+    defaultSearchProviderEnabled = true;
+    defaultSearchProviderSearchURL = "${search-url}/search?q={searchTerms}";
+    defaultSearchProviderSuggestURL = "${search-url}/complete/search?output=chrome&q={searchTerms}";
     extensions = [
       "dbepggeogbaibhgnhhndojpepiihcmeb" # vimium
       "knheggckgoiihginacbkhaalnibhilkk" # notion-web-clipper
@@ -13,7 +21,85 @@ lib.mkProfile s "desktop-apps"
       "kdbmhfkmnlmbkgbabkdealhhbfhlmmon" # steamdb
       "ngonfifpkpeefnhelnfdkficaiihklid" # protondb-for-steam
       "gebbhagfogifgggkldgodflihgfeippi" # return-youtube-dislike
+      "lkbebcjgcmobigpeffafkodonchffocl;https://gitflic.ru/project/magnolia1234/bpc_updates/blob/raw?file=updates.xml" # bypass-paywall
+
     ];
+    extraOpts = {
+      BraveRewardsDisabled = true;
+      BraveWalletDisabled = true;
+      BraveVPNDisabled = true;
+      BraveAIChatEnabled = false;
+      TorDisabled = true;
+
+      PasswordManagerEnabled = false;
+      PasswordSharingEnabled = false;
+      PasswordLeakDetectionEnabled = false;
+
+      MetricsReportingEnabled = false;
+      SafeBrowsingExtendedReportingEnabled = false;
+      SafeBrowsingSurveysEnabled = false;
+      SafeBrowsingDeepScanningEnabled = false;
+
+      NewTabPageLocation = search-url;
+
+      SiteSearchSettings = [
+        {
+          name = "Nix Code";
+          shortcut = "nc";
+          url = "https://github.com/search?q={searchTerms}+NOT+is%3Afork+language%3ANix&type=code";
+        }
+        {
+          name = "NixOS Packages";
+          shortcut = "np";
+          url = "https://search.nixos.org/packages?channel=unstable&query={searchTerms}";
+        }
+        {
+          name = "NixOS Options";
+          shortcut = "no";
+          url = "https://search.nixos.org/options?channel=unstable&query={searchTerms}";
+        }
+        {
+          name = "NixOS Wiki";
+          shortcut = "nw";
+          url = "https://wiki.nixos.org/w/index.php?search={searchTerms}";
+        }
+        {
+          name = "Home Manager Options";
+          shortcut = "ho";
+          url = "https://home-manager-options.extranix.com/?query={searchTerms}&release=master";
+        }
+        {
+          name = "Nixpkgs GitHub";
+          shortcut = "ni";
+          url = "https://github.com/search?q=repo%3ANixOS%2Fnixpkgs%20{searchTerms}";
+        }
+        {
+          name = "ProtonDB";
+          shortcut = "pd";
+          url = "https://protondb.com/search?q={searchTerms}";
+        }
+        {
+          name = "Steam Store";
+          shortcut = "steam";
+          url = "https://store.steampowered.com/search/?term={searchTerms}";
+        }
+        {
+          name = "Wikipedia";
+          shortcut = "wiki";
+          url = "https://en.wikipedia.org/w/index.php?search={searchTerms}";
+        }
+        {
+          name = "Reddit";
+          shortcut = "red";
+          url = "https://old.reddit.com/search?q={searchTerms}&include_over_18=on";
+        }
+        {
+          name = "Arch Wiki";
+          shortcut = "arch";
+          url = "https://wiki.archlinux.org/index.php?search={searchTerms}";
+        }
+      ];
+    };
   };
 
   myhome = { config, ... }:
