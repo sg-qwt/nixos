58e11b029c4b40cee60f896b1ca97fb4e5e98a3f
無名氏 <hello@edgerunners.eu.org>
2025-06-27 13:52:33 +0800

    chore: refactor & ssh sign

--- hosts/li/configuration.nix
+++ hosts/li/configuration.nix
@@ -1,23 +1,17 @@
 { config, pkgs, lib, ... }:
 {
-
-  myos.asusd.enable = true;
-
-  myos.sway.enable = true;
-
-  myos.tmux.enable = true;
-  myos.qqqemacs.enable = true;
-
-  myos.shell.enable = true;
-  myos.git.enable = true;
-  myos.ssh.enable = true;
-  myos.desktop-apps.enable = true;
-  myos.tools.enable = true;
-
-  myos.clash-meta.enable = true;
-  myos.tailscale.enable = true;
-  myos.android.enable = true;
-
-  myos.container.enable = true;
-
+  myos = {
+    asusd.enable = true;
+    sway.enable = true;
+    tmux.enable = true;
+    qqqemacs.enable = true;
+    shell.enable = true;
+    git.enable = true;
+    ssh.enable = true;
+    desktop-apps.enable = true;
+    tools.enable = true;
+    clash-meta.enable = true;
+    tailscale.enable = true;
+    android.enable = true;
+  };
 }
