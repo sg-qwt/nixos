20b2f1fe45399eb6bcb011822cd1c175cc51f8fe
無名氏 <hello@edgerunners.eu.org>
2025-07-01 17:44:54 +0800

    chore: tweak gaming, add mangohud

--- modules/profiles/gaming.nix
+++ modules/profiles/gaming.nix
@@ -1,6 +1,19 @@
 s@{ config, pkgs, lib, self, ... }:
+let
+  myhomecfg = config.home-manager.users."${config.myos.user.mainUser}";
+in
 lib.mkProfile s "gaming"
 {
+  # TODO move nvidia-offload to gamescope here once issue fixed
+  # https://github.com/ValveSoftware/gamescope/issues/1590
+  programs.gamescope = {
+    enable = true;
+    capSysNice = true;
+    env = {
+      MANGOHUD_CONFIGFILE = myhomecfg.xdg.configFile."MangoHud/MangoHud.conf".target;
+    };
+  };
+
   programs.steam = {
     enable = true;
     fontPackages = with pkgs; [ noto-fonts-cjk-sans ];
@@ -24,11 +37,34 @@ lib.mkProfile s "gaming"
         "3840"
         "--output-height"
         "2160"
+        "--mangoapp"
       ];
       steamArgs = [
         "-tenfoot"
         "-pipewire-dmabuf"
       ];
+    };
+  };
+
+  myhome = { config, lib, osConfig, ... }: {
+    programs.mangohud = {
+      enable = true;
+      settings = {
+        pci_dev = "0000:01:00.0";
+        horizontal = true;
+        horizontal_stretch = false;
+        hud_no_margin = true;
+        fps = true;
+        cpu_stats = true;
+        cpu_temp = true;
+        gpu_stats = true;
+        gpu_temp = true;
+        ram = true;
+        vram = true;
+        hud_compact = true;
+        toggle_hud = "F12";
+        toggle_hud_position = "F11";
+      };
     };
   };
 
