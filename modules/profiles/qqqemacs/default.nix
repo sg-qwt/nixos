12312b2c6a140fe94032c8bfdd440e94d9189668
無名氏 <hello@edgerunners.eu.org>
2025-10-20 03:49:11 +0800

    feat: play around eca

--- modules/profiles/qqqemacs/default.nix
+++ modules/profiles/qqqemacs/default.nix
@@ -21,6 +21,10 @@ lib.mkProfile s "qqqemacs"
         owner = config.myos.user.mainUser;
       };
 
+      vaultix.secrets.eca-openai-key = {
+        owner = config.myos.user.mainUser;
+      };
+
       myos.sdcv-with-dicts.enable = true;
 
       myos.langs = {
@@ -34,10 +38,35 @@ lib.mkProfile s "qqqemacs"
         ];
       };
 
-      myhome = { config, ... }: {
+      myhome = { config, osConfig, ... }: {
         home.sessionVariables.EDITOR = "emacsclient -t";
 
+        xdg.configFile."eca/config.json" = {
+          text = builtins.toJSON {
+            defaultBehavior = "plan";
+            defaultModel = "azure/gpt-4o";
+            providers = {
+              azure = {
+                api = "openai-chat";
+                url = "https://${gpt-host}";
+                keyEnv = "ECA_AZ_OPENAI_KEY";
+                completionUrlRelativePath = "/openai/deployments/shuqi/chat/completions?api-version=2025-01-01-preview";
+                models = {
+                  gpt-4o = {
+                    extraPayload = {
+                      max_completion_tokens = 16000;
+                    };
+                  };
+                };
+              };
+            };
+          };
+        };
+
         programs.bash.initExtra = ''
+
+          export ECA_AZ_OPENAI_KEY="$(cat ${osConfig.vaultix.secrets.eca-openai-key.path})"
+
           if [[ "$INSIDE_EMACS" = 'vterm' ]]; then
             source ${pkgs.emacsPackages.vterm}/share/emacs/site-lisp/elpa/vterm-*/etc/emacs-vterm-bash.sh
           fi
