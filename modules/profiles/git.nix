4bb46884696adbda911a4d6b5fd3dc9dbe6a42e1
無名氏 <hello@edgerunners.eu.org>
2025-10-31 01:00:24 +0800

    git: add caveman token

--- modules/profiles/git.nix
+++ modules/profiles/git.nix
@@ -1,6 +1,14 @@
 s@{ config, pkgs, lib, ... }:
 lib.mkProfile s "git"
 {
+  vaultix.secrets.caveman-token = { };
+  vaultix.templates.github-caveman-conf = {
+    owner = config.myos.user.mainUser;
+    content = ''
+      [url "https://${config.vaultix.placeholder.caveman-token}@github.com/sg-qwt/caveman"]
+          insteadOf = https://github.com/sg-qwt/caveman
+    '';
+  };
   myhome = {
     programs.git = {
       enable = true;
@@ -24,6 +32,10 @@ lib.mkProfile s "git"
       };
 
       ignores = [ ".lsp/.cache" ".clj-kondo/.cache" ];
+
+      includes = [
+        { path = config.vaultix.templates.github-caveman-conf.path; }
+      ];
     };
   };
 }
