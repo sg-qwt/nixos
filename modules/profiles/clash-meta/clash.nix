d904a27ac6af150c02ae09f8ec6e08b57f21bccb
無名氏 <hello@edgerunners.eu.org>
2026-06-19 12:11:28 +0800

    fix: openai rule

--- modules/profiles/clash-meta/clash.nix
+++ modules/profiles/clash-meta/clash.nix
@@ -212,6 +212,8 @@ rec {
     "DOMAIN-SUFFIX,steamserver.net,DIRECT"
     "DOMAIN-SUFFIX,steamchina.com,DIRECT"
     "DOMAIN-SUFFIX,polymarket.com,anytls"
+    "DOMAIN-SUFFIX,openai.com,sstls"
+    "DOMAIN-SUFFIX,chatgpt.com,sstls"
     "GEOSITE,category-ads-all,REJECT"
     "GEOSITE,openai,select"
     "GEOSITE,geolocation-cn,DIRECT"
